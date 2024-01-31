cf99834bc525b028c3e09809178a4aad011a92fc
OlesStankevych <oles@trainual.com>
2024-01-31 19:29:16 +0200

    setup routers and mui joy

---
+++ app/javascript/react/src/components/layout/NavBar.tsx
@@ -0,0 +1,30 @@
+import { Link as RouterLink } from 'react-router-dom';
+import { Sheet, Link } from '@mui/joy';
+
+const NavBar = () => {
+  return (
+    <Sheet
+      sx={{
+        display: 'flex',
+        justifyContent: 'space-around',
+        alignItems: 'center',
+        padding: '1rem',
+        backgroundColor: 'background.body',
+        boxShadow: 'md',
+      }}
+    >
+      <Link component={RouterLink} to="/" sx={{ textDecoration: 'none' }}>
+        Home
+      </Link>
+      <Link component={RouterLink} to="/about" sx={{ textDecoration: 'none' }}>
+        About
+      </Link>
+      <Link component={RouterLink} to="/contact" sx={{ textDecoration: 'none' }}>
+        Contact
+      </Link>
+      {/* Add more links as needed */}
+    </Sheet>
+  );
+};
+
+export default NavBar;
--- app/javascript/react/src/main.tsx
+++ app/javascript/react/src/main.tsx
@@ -1,10 +1,29 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
+import Root from "./routes/Root";
 import './index.css'
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Root />,
+      },
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
