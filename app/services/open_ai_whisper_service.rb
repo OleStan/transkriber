64cbced83a28c565ac54e726c971f607ebd11f62
OlesStankevych <oles@trainual.com>
2025-04-21 21:57:08 +0300

    Update dependencies and improve OpenAI configuration

--- app/services/open_ai_whisper_service.rb
+++ app/services/open_ai_whisper_service.rb
@@ -95,8 +95,7 @@ class OpenAiWhisperService < ApplicationService
   end
 
   def transcribe_audio(temp_file, response_format)
-    return mocked_response if Rails.env.development?
-
+    # return mocked_response if Rails.env.development?
     response = client.audio.transcribe(
       parameters: {
         language: @language,
